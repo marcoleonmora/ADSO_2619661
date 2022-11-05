ae55fc57b86624b4a3068cbf18c241135d927a11
marcoleonmora <mlmoram@misena.edu.co>
2022-11-04 20:37:15 -0500

    Ejercicios Python con funciones

--- ejerciciosPython/matriz.py
+++ ejerciciosPython/matriz.py
@@ -23,68 +23,82 @@ for x in range(len(matriz)):
         #generar numero al azar entre 10 y 99.
         matriz[x][y] = random.randrange(10, 99)
             
+long = len(matriz)
 
 #mostrarla
-for x in range(len(matriz)):
-    print(matriz[x])
+def mostrarMatriz():
+    for x in range(len(matriz)):
+        print(matriz[x])
+
 
 
 #MOSTRAR DIAGONAL PRINCIPAL
-print()
-print('----- DIAGONAL PRINCIPAL --------')
-long = len(matriz)
-for x in range(long):
-    print('    ' * x, end="")
-    print(matriz[x][x], end=", ")
+def mostrarDiagPpal():
     print()
+    print('----- DIAGONAL PRINCIPAL --------')
+    
+    for x in range(long):
+        print('    ' * x, end="")
+        print(matriz[x][x], end=", ")
+        print()
 
     
 #MOSTRAR DIAGONAL INVERSA
-print()
-print('----- DIAGONAL INVERSA --------')
-for f in range(long):
-    c = long- 1 - f
-    print('    ' * c, end='')
-    print(matriz[f][c], end=",")
+def mostrarDiagInv():
     print()
+    print('----- DIAGONAL INVERSA --------')
+    for f in range(long):
+        c = long- 1 - f
+        print('    ' * c, end='')
+        print(matriz[f][c], end=",")
+        print()
 
 
 #MOSTRAR SUPERIOR DERECHO
-print()
-print('----- MATRIZ SUPERIOR DERECHA --------')
-for f in range(long):
-    print('    ' * f, end='')
-    for c in range(f, long):
-        print(matriz[f][c], end=", ")
+def mostrarSupDer():
     print()
+    print('----- MATRIZ SUPERIOR DERECHA --------')
+    for f in range(long):
+        print('    ' * f, end='')
+        for c in range(f, long):
+            print(matriz[f][c], end=", ")
+        print()
 
 
 #MOSTRAR SUPERIOR IZQUIERDO
-print()
-print('----- MATRIZ SUPERIOR IZQUIERDA --------')
-for f in range(long):
-    for c in range(long - f):
-        print(matriz[f][c], end=", ")
+def mostrarSupIzq():
     print()
+    print('----- MATRIZ SUPERIOR IZQUIERDA --------')
+    for f in range(long):
+        for c in range(long - f):
+            print(matriz[f][c], end=", ")
+        print()
 
 
 #MOSTRAR INFERIOR DERECHA
-print()
-print('----- MATRIZ INFERIOR DERECHA --------')
-for f in range(long):
-    print('    ' * (long- f-1), end= '')
-    for c in range(long - f-1, long):
-        print(matriz[f][c], end=", ")
+def mostrarInfDer():
     print()
+    print('----- MATRIZ INFERIOR DERECHA --------')
+    for f in range(long):
+        print('    ' * (long- f-1), end= '')
+        for c in range(long - f-1, long):
+            print(matriz[f][c], end=", ")
+        print()
 
 
 #MOSTRAR INFERIOR IZQUIERDA
-print()
-print('----- MATRIZ INFERIOR IZQUIERDA --------')
-for f in range(long):
-    for c in range(f+1):
-        print(matriz[f][c], end=", ")
+def mostrarInfIzq():
     print()
+    print('----- MATRIZ INFERIOR IZQUIERDA --------')
+    for f in range(long):
+        for c in range(f+1):
+            print(matriz[f][c], end=", ")
+        print()
+
+
+
+print('++*************************')
+mostrarMatriz()
     
 
 
